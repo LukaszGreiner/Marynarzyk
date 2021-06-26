31372bd3944ba8f17052ee2c9690572553d4b1f8
xKaktus <devlukaszgreiner@gmail.com>
2021-06-26 21:59:17 +0200

    Refactor, arrays

--- js/marynarzyk.js
+++ js/marynarzyk.js
@@ -1,13 +1,13 @@
 // gra w marynarzyka
 
 //pobieranie danych
-
 let playGame = confirm("Zagramy partyjke?");
 if(playGame) {
     //play
-    let playerChoice = prompt("Wybierz kamień, papier lub nożyce!")
-    if(playerChoice) {
-        let player1 = playerChoice.trim().toLowerCase();
+    while (playGame) {
+    const playerChoice = prompt("Wybierz kamień, papier lub nożyce!")
+    if(playerChoice || playerChoice === "") {
+        const player1 = playerChoice.trim().toLowerCase();
         if (
             player1 === "kamień" ||
             player1 === "papier" ||
@@ -16,20 +16,11 @@ if(playGame) {
             //gra
             
 //losowanie przez komputer
-let computer = Math.floor(Math.random() * 3) + 1;
+const computerChoice = Math.floor(Math.random() * 3);
+const kpnArray = ["kamień", "papier", "nożyce"];
+const player2 = kpnArray[computerChoice];
 
-let player2 =
-computer === 1 
-? "kamień"
-: computer === 2
-? "papier"
-: computer === 3
-? "nożyce"
-: "Error"
-
-//alert(`Komputer wylosował ${player2}`);
-
-let result =
+const result =
 //tie game
 player1 === player2
 ? "Remis"
@@ -46,16 +37,23 @@ player1 === player2
 : player1 === "nożyce" && player2 === "papier"
 ? "Wygrałeś!"
 : "Przegrałeś :("
+
+//alert
 alert(`Ty: ${player1}\nKomputer: ${player2}\n${result}`);
+
 //play again
-let playAgain = confirm("Chcesz zagrać jeszcze raz?");
-playAgain ? location.reload() : alert("Ok, bywaj");
-        }else {
+playGame = confirm("Zagramy jeszcze raz?");
+if (!playGame) alert ("Szkoda, spróbujmy kiedy indziej.")
+continue;
+}else {
             alert("Nie podałeś poprawnego ruchu.");
+            continue;
         }
     }else {
         alert("Szkoda, spróbujmy kiedy indziej.")
+        break;
     }
+}
 }else {
     alert("Ok, może następnym razem.")
 }
